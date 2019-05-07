aa6f7140aa5db08da20817a6560d416ab0b7fb3c
Nev Flynn <nev@recroot.ie>
2019-05-07 14:20:18 +0100

    Mobile menu header

---
+++ js/main.js
@@ -0,0 +1,9 @@
+//Hamburger functionality
+$(document).ready(function () {
+        $(".hamburger-container").click(function () {
+            $(".hamburger").toggleClass("is-active");
+            $(".mobile-menu").toggleClass("menu-active");
+        });
+    });
+    
+
