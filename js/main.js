5db8c99e9745f34b190fb105b5a7023bc028e551
Nev Flynn <nev@recroot.ie>
2019-05-15 12:31:06 +0100

    How it works page

--- js/main.js
+++ js/main.js
@@ -7,3 +7,14 @@ $(document).ready(function () {
     });
     
 
+    $(document).ready(function () {
+        $('.carousel-container').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: true,
+            dots:true,
+            adaptiveHeight: true
+        });
+    });
+    
+
